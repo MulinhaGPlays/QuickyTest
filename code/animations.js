89a31c3470d0afd41c7e96994f0cf85af607985e
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2023-06-08 11:42:16 -0300

    animação inicial readicionada

---
+++ code/animations.js
@@ -0,0 +1,12 @@
+var content = document.getElementById('content');
+var loadingScreen = document.getElementById('loading-screen');
+
+function removeLoadingScreen() { 
+    loadingScreen.classList.add('finalizado');
+    setInterval(() => {
+        loadingScreen.style.display = 'none';
+        content.style.display = 'block';
+    }, 1900)
+}
+
+document.addEventListener('DOMContentLoaded', removeLoadingScreen())
